584c9daaf6f7cf06bd9696fca4d3947e7294884f
Emmanue <kioxmanu7@gmail.com>
2023-10-25 21:15:26 +0300

    assignment done

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 
-function Form(props) {
+function Form() {
   const [firstName, setFirstName] = useState("Sylvia");
   const [lastName, setLastName] = useState("Woods");
+  const [submittedData, setSubmittedData] = useState([]);
 
   function handleFirstNameChange(event) {
     setFirstName(event.target.value);
@@ -12,12 +13,37 @@ function Form(props) {
     setLastName(event.target.value);
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const formData = {
+      firstName: firstName,
+      lastName: lastName,
+    };
+
+    setSubmittedData([...submittedData, formData]);
+
+    setFirstName("");
+    setLastName("");
+  }
+
   return (
-    <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
-      <button type="submit">Submit</button>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={handleFirstNameChange} value={firstName} />
+        <input type="text" onChange={handleLastNameChange} value={lastName} />
+        <button type="submit">Submit</button>
+      </form>
+
+      <h3>Submissions</h3>
+      <ul>
+        {submittedData.map((data, index) => (
+          <li key={index}>
+            {data.firstName} {data.lastName}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
